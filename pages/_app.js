528939461b1b4f277403cafb6bab6845d3b295b7
Akshay Urs <84136865+akshayurs@users.noreply.github.com>
2021-11-21 22:08:31 +0530

    analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
-
+import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NNKV85BRMQ"
+      ></Script>
+      <Script id="analytics">
+        {`
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-NNKV85BRMQ')
+            `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
